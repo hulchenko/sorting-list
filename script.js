9b5fcbb637a3742a61aa617207d379e854c1010e
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-23 13:49:19 -0700

    add css, update html, start js

---
+++ script.js
@@ -0,0 +1,15 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) { //#2 replacing articles with empty string and trim spaces.
+    return bandName.replace(/^(a |the |an)/i, "").trim();
+}
+const sortedBands = bands.sort(function(a, b) { //#1 alphabetizing list
+    if (strip(a) > strip(b)) {
+        return 1;
+    } else {
+        return -1;
+    }
+    ///OR ternary operator///
+    //return strip(a)>strip(b) ? 1 : -1;
+});
+console.log(sortedBands);
